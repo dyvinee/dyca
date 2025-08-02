b5d697686646fd2f1741713bc46f83ddc11455cb
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-08-03 02:22:30 +0700

    feat: update.

--- src/core/eval.d
+++ src/core/eval.d
@@ -4,20 +4,91 @@ import core.ast;
 import core.object;
 import std.container : DList;
 import std.conv : to;
-import std.string : format;
+import std.format : format;
+import std.string : join;
 
 class Evaluator {
     static Object eval(Node node, Environment env) {
         if (auto prog = cast(Program)node) {
             return evalProgram(prog, env);
         }
-        else if (auto stmt = cast(Statement)node) {
-            return evalStatement(stmt, env);
+        else if (auto stmt = cast(LetStatement)node) {
+            Object val = eval(stmt.value, env);
+            if (isError(val)) return val;
+            env.set(stmt.name.value, val);
         }
-        else if (auto expr = cast(Expression)node) {
-            return evalExpression(expr, env);
+        else if (auto stmt = cast(ReturnStatement)node) {
+            Object val = eval(stmt.returnValue, env);
+            if (isError(val)) return val;
+            return new ReturnValue(val);
         }
-        return new Null();
+        else if (auto stmt = cast(ExpressionStatement)node) {
+            return eval(stmt.expression, env);
+        }
+        else if (auto stmt = cast(BlockStatement)node) {
+            return evalBlockStatement(stmt, env);
+        }
+        else if (auto expr = cast(IntegerLiteral)node) {
+            return new Integer(expr.value);
+        }
+        else if (auto expr = cast(BooleanLiteral)node) {
+            return nativeBoolToBooleanObject(expr.value);
+        }
+        else if (auto expr = cast(StringLiteral)node) {
+            return new String(expr.value);
+        }
+        else if (auto expr = cast(ArrayLiteral)node) {
+            Object[] elements = evalExpressions(expr.elements, env);
+            if (elements.length == 1 && isError(elements[0])) {
+                return elements[0];
+            }
+            return new Array(elements);
+        }
+        else if (auto expr = cast(Identifier)node) {
+            return evalIdentifier(expr, env);
+        }
+        else if (auto expr = cast(PrefixExpression)node) {
+            Object right = eval(expr.right, env);
+            if (isError(right)) return right;
+            return evalPrefixExpression(expr.operator, right);
+        }
+        else if (auto expr = cast(InfixExpression)node) {
+            Object left = eval(expr.left, env);
+            if (isError(left)) return left;
+            
+            Object right = eval(expr.right, env);
+            if (isError(right)) return right;
+            
+            return evalInfixExpression(expr.operator, left, right);
+        }
+        else if (auto expr = cast(IfExpression)node) {
+            return evalIfExpression(expr, env);
+        }
+        else if (auto expr = cast(FunctionLiteral)node) {
+            return new Function(expr.parameters, expr.body, env);
+        }
+        else if (auto expr = cast(CallExpression)node) {
+            Object function = eval(expr.function, env);
+            if (isError(function)) return function;
+            
+            Object[] args = evalExpressions(expr.arguments, env);
+            if (args.length == 1 && isError(args[0])) {
+                return args[0];
+            }
+            
+            return applyFunction(function, args);
+        }
+        else if (auto expr = cast(IndexExpression)node) {
+            Object left = eval(expr.left, env);
+            if (isError(left)) return left;
+            
+            Object index = eval(expr.index, env);
+            if (isError(index)) return index;
+            
+            return evalIndexExpression(left, index);
+        }
+        
+        return null;
     }
 
     static Object evalProgram(Program program, Environment env) {
@@ -37,77 +108,20 @@ class Evaluator {
         return result;
     }
 
-    static Object evalStatement(Statement stmt, Environment env) {
-        if (auto exprStmt = cast(ExpressionStatement)stmt) {
-            return eval(exprStmt.expression, env);
-        }
-        else if (auto block = cast(BlockStatement)stmt) {
-            return evalBlockStatement(block, env);
-        }
-        else if (auto ret = cast(ReturnStatement)stmt) {
-            Object val = eval(ret.returnValue, env);
-            if (isError(val)) return val;
-            return new ReturnValue(val);
-        }
-        else if (auto let = cast(LetStatement)stmt) {
-            Object val = eval(let.value, env);
-            if (isError(val)) return val;
-            env.set(let.name.value, val);
-        }
-        return new Null();
-    }
-
-    static Object evalExpression(Expression expr, Environment env) {
-        if (auto ident = cast(Identifier)expr) {
-            return evalIdentifier(ident, env);
-        }
-        else if (auto lit = cast(IntegerLiteral)expr) {
-            return new Integer(lit.value);
-        }
-        else if (auto boolLit = cast(BooleanLiteral)expr) {
-            return nativeBoolToBooleanObject(boolLit.value);
-        }
-        else if (auto prefix = cast(PrefixExpression)expr) {
-            Object right = eval(prefix.right, env);
-            if (isError(right)) return right;
-            return evalPrefixExpression(prefix.operator, right);
-        }
-        else if (auto infix = cast(InfixExpression)expr) {
-            Object left = eval(infix.left, env);
-            if (isError(left)) return left;
+    static Object evalBlockStatement(BlockStatement block, Environment env) {
+        Object result;
+        
+        foreach (stmt; block.statements) {
+            result = eval(stmt, env);
             
-            Object right = eval(infix.right, env);
-            if (isError(right)) return right;
-            
-            return evalInfixExpression(infix.operator, left, right);
-        }
-        else if (auto ifExpr = cast(IfExpression)expr) {
-            return evalIfExpression(ifExpr, env);
-        }
-        else if (auto call = cast(CallExpression)expr) {
-            Object function = eval(call.function, env);
-            if (isError(function)) return function;
-            
-            Object[] args = evalExpressions(call.arguments, env);
-            if (args.length == 1 && isError(args[0])) {
-                return args[0];
+            if (result !is null && 
+                (result.objectType() == "RETURN_VALUE" || 
+                 result.objectType() == "ERROR")) {
+                return result;
             }
-            
-            return applyFunction(function, args);
-        }
-        return new Null();
-    }
-
-    static Object evalIdentifier(Identifier node, Environment env) {
-        if (auto val = env.get(node.value)) {
-            return val;
         }
         
-        if (auto builtin = builtins.get(node.value, null)) {
-            return builtin;
-        }
-        
-        return new Error("identifier not found: " ~ node.value);
+        return result;
     }
 
     static Object evalPrefixExpression(string op, Object right) {
@@ -121,9 +135,27 @@ class Evaluator {
         }
     }
 
+    static Object evalBangOperatorExpression(Object right) {
+        if (right is NULL) return new Boolean(true);
+        if (right is FALSE) return new Boolean(true);
+        return new Boolean(false);
+    }
+
+    static Object evalMinusPrefixOperatorExpression(Object right) {
+        if (right.objectType() != "INTEGER") {
+            return new Error("unknown operator: -%s".format(right.objectType()));
+        }
+        
+        long value = (cast(Integer)right).value;
+        return new Integer(-value);
+    }
+
     static Object evalInfixExpression(string op, Object left, Object right) {
         if (left.objectType() == "INTEGER" && right.objectType() == "INTEGER") {
-            return evalIntegerInfixExpression(op, cast(Integer)left, cast(Integer)right);
+            return evalIntegerInfixExpression(op, left, right);
+        }
+        else if (left.objectType() == "STRING" && right.objectType() == "STRING") {
+            return evalStringInfixExpression(op, left, right);
         }
         else if (op == "==") {
             return nativeBoolToBooleanObject(left == right);
@@ -141,41 +173,205 @@ class Evaluator {
         }
     }
 
-    static Object evalIntegerInfixExpression(string op, Integer left, Integer right) {
+    static Object evalIntegerInfixExpression(string op, Object left, Object right) {
+        long leftVal = (cast(Integer)left).value;
+        long rightVal = (cast(Integer)right).value;
+        
         switch (op) {
             case "+":
-                return new Integer(left.value + right.value);
+                return new Integer(leftVal + rightVal);
             case "-":
-                return new Integer(left.value - right.value);
+                return new Integer(leftVal - rightVal);
             case "*":
-                return new Integer(left.value * right.value);
+                return new Integer(leftVal * rightVal);
             case "/":
-                return new Integer(left.value / right.value);
+                return new Integer(leftVal / rightVal);
             case "<":
-                return nativeBoolToBooleanObject(left.value < right.value);
+                return nativeBoolToBooleanObject(leftVal < rightVal);
             case ">":
-                return nativeBoolToBooleanObject(left.value > right.value);
+                return nativeBoolToBooleanObject(leftVal > rightVal);
             case "==":
-                return nativeBoolToBooleanObject(left.value == right.value);
+                return nativeBoolToBooleanObject(leftVal == rightVal);
             case "!=":
-                return nativeBoolToBooleanObject(left.value != right.value);
+                return nativeBoolToBooleanObject(leftVal != rightVal);
             default:
                 return new Error("unknown operator: %s %s %s".format(
                     left.objectType(), op, right.objectType()));
         }
     }
 
+    static Object evalStringInfixExpression(string op, Object left, Object right) {
+        if (op != "+") {
+            return new Error("unknown operator: %s %s %s".format(
+                left.objectType(), op, right.objectType()));
+        }
+        
+        string leftVal = (cast(String)left).value;
+        string rightVal = (cast(String)right).value;
+        return new String(leftVal ~ rightVal);
+    }
+
+    static Object evalIfExpression(IfExpression expr, Environment env) {
+        Object condition = eval(expr.condition, env);
+        if (isError(condition)) return condition;
+        
+        if (isTruthy(condition)) {
+            return eval(expr.consequence, env);
+        } else if (expr.alternative !is null) {
+            return eval(expr.alternative, env);
+        } else {
+            return NULL;
+        }
+    }
+
+    static Object evalIdentifier(Identifier node, Environment env) {
+        if (auto val = env.get(node.value)) {
+            return val;
+        }
+        
+        if (auto builtin = builtins.get(node.value, null)) {
+            return builtin;
+        }
+        
+        return new Error("identifier not found: " ~ node.value);
+    }
+
+    static Object[] evalExpressions(Expression[] exprs, Environment env) {
+        Object[] result;
+        
+        foreach (expr; exprs) {
+            Object evaluated = eval(expr, env);
+            if (isError(evaluated)) {
+                return [evaluated];
+            }
+            result ~= evaluated;
+        }
+        
+        return result;
+    }
+
+    static Object applyFunction(Object fn, Object[] args) {
+        if (auto function = cast(Function)fn) {
+            Environment extendedEnv = extendFunctionEnv(function, args);
+            Object evaluated = eval(function.body, extendedEnv);
+            return unwrapReturnValue(evaluated);
+        }
+        else if (auto builtin = cast(Builtin)fn) {
+            return builtin.call(args);
+        }
+        else {
+            return new Error("not a function: %s".format(fn.objectType()));
+        }
+    }
+
+    static Environment extendFunctionEnv(Function fn, Object[] args) {
+        Environment env = new Environment(fn.env);
+        
+        for (size_t i = 0; i < fn.parameters.length; i++) {
+            env.set(fn.parameters[i].value, args[i]);
+        }
+        
+        return env;
+    }
+
+    static Object evalIndexExpression(Object left, Object index) {
+        if (left.objectType() == "ARRAY" && index.objectType() == "INTEGER") {
+            return evalArrayIndexExpression(left, index);
+        }
+        return new Error("index operator not supported: %s".format(left.objectType()));
+    }
+
+    static Object evalArrayIndexExpression(Object array, Object index) {
+        Object[] elements = (cast(Array)array).elements;
+        long idx = (cast(Integer)index).value;
+        
+        if (idx < 0 || idx >= elements.length) {
+            return NULL;
+        }
+        
+        return elements[idx];
+    }
+
+    static Object unwrapReturnValue(Object obj) {
+        if (auto returnVal = cast(ReturnValue)obj) {
+            return returnVal.value;
+        }
+        return obj;
+    }
+
     static Boolean nativeBoolToBooleanObject(bool input) {
-        return input ? new Boolean(true) : new Boolean(false);
+        return input ? TRUE : FALSE;
     }
 
     static bool isTruthy(Object obj) {
-        if (obj is null) return false;
-        if (auto b = cast(Boolean)obj) return b.value;
+        if (obj is NULL) return false;
+        if (obj is FALSE) return false;
+        if (obj is TRUE) return true;
         return true;
     }
 
     static bool isError(Object obj) {
         return obj !is null && obj.objectType() == "ERROR";
+    }
+
+    // Built-in constants
+    static Null NULL = new Null();
+    static Boolean TRUE = new Boolean(true);
+    static Boolean FALSE = new Boolean(false);
+
+    // Built-in functions
+    static Builtin[string] builtins = [
+        "len": new LenFunction(),
+        "first": new FirstFunction(),
+        "last": new LastFunction(),
+        "rest": new RestFunction(),
+        "push": new PushFunction(),
+        "puts": new PutsFunction()
+    ];
+}
+
+// Built-in function implementations
+class LenFunction : Builtin {
+    override Object call(Object[] args) {
+        if (args.length != 1) {
+            return new Error("wrong number of arguments. got=%d, want=1".format(args.length));
+        }
+        
+        if (auto str = cast(String)args[0]) {
+            return new Integer(str.value.length);
+        }
+        else if (auto arr = cast(Array)args[0]) {
+            return new Integer(arr.elements.length);
+        }
+        
+        return new Error("argument to `len` not supported, got %s".format(args[0].objectType()));
+    }
+}
+
+class FirstFunction : Builtin {
+    override Object call(Object[] args) {
+        if (args.length != 1) {
+            return new Error("wrong number of arguments. got=%d, want=1".format(args.length));
+        }
+        
+        if (args[0].objectType() != "ARRAY") {
+            return new Error("argument to `first` must be ARRAY, got %s".format(args[0].objectType()));
+        }
+        
+        Object[] arr = (cast(Array)args[0]).elements;
+        if (arr.length > 0) {
+            return arr[0];
+        }
+        
+        return Evaluator.NULL;
+    }
+}
+
+class PutsFunction : Builtin {
+    override Object call(Object[] args) {
+        foreach (arg; args) {
+            writeln(arg.inspect());
+        }
+        return Evaluator.NULL;
     }
 }
--- src/core/parser.d
+++ src/core/parser.d
@@ -4,11 +4,13 @@ import std.array : array;
 import std.algorithm : map;
 import std.exception : enforce;
 import std.format : format;
+import std.conv : to;
 import syntax.token : Token, TokenType;
 import core.ast : Program, Statement, Expression, LetStatement, ReturnStatement,
                   ExpressionStatement, BlockStatement, Identifier, IntegerLiteral,
                   BooleanLiteral, PrefixExpression, InfixExpression, IfExpression,
-                  FunctionLiteral, CallExpression, ImportStatement, ExportStatement;
+                  FunctionLiteral, CallExpression, ImportStatement, ExportStatement,
+                  StringLiteral, ArrayLiteral, IndexExpression;
 
 class Parser {
     Lexer lexer;
@@ -23,32 +25,39 @@ class Parser {
         this.lexer = lexer;
         this.errors = [];
         
-        // Initialize prefix parse functions
-        prefixParseFns[TokenType.IDENT] = "parseIdentifier";
-        prefixParseFns[TokenType.INT] = "parseIntegerLiteral";
-        prefixParseFns[TokenType.TRUE] = "parseBooleanLiteral";
-        prefixParseFns[TokenType.FALSE] = "parseBooleanLiteral";
-        prefixParseFns[TokenType.BANG] = "parsePrefixExpression";
-        prefixParseFns[TokenType.MINUS] = "parsePrefixExpression";
-        prefixParseFns[TokenType.LPAREN] = "parseGroupedExpression";
-        prefixParseFns[TokenType.IF] = "parseIfExpression";
-        prefixParseFns[TokenType.FUNCTION] = "parseFunctionLiteral";
-        prefixParseFns[TokenType.STRING] = "parseStringLiteral";
+        registerPrefix(TokenType.IDENT, "parseIdentifier");
+        registerPrefix(TokenType.INT, "parseIntegerLiteral");
+        registerPrefix(TokenType.TRUE, "parseBooleanLiteral");
+        registerPrefix(TokenType.FALSE, "parseBooleanLiteral");
+        registerPrefix(TokenType.BANG, "parsePrefixExpression");
+        registerPrefix(TokenType.MINUS, "parsePrefixExpression");
+        registerPrefix(TokenType.LPAREN, "parseGroupedExpression");
+        registerPrefix(TokenType.IF, "parseIfExpression");
+        registerPrefix(TokenType.FUNCTION, "parseFunctionLiteral");
+        registerPrefix(TokenType.STRING, "parseStringLiteral");
+        registerPrefix(TokenType.LBRACKET, "parseArrayLiteral");
         
-        // Initialize infix parse functions
-        infixParseFns[TokenType.PLUS] = "parseInfixExpression";
-        infixParseFns[TokenType.MINUS] = "parseInfixExpression";
-        infixParseFns[TokenType.SLASH] = "parseInfixExpression";
-        infixParseFns[TokenType.ASTERISK] = "parseInfixExpression";
-        infixParseFns[TokenType.EQ] = "parseInfixExpression";
-        infixParseFns[TokenType.NOT_EQ] = "parseInfixExpression";
-        infixParseFns[TokenType.LT] = "parseInfixExpression";
-        infixParseFns[TokenType.GT] = "parseInfixExpression";
-        infixParseFns[TokenType.LPAREN] = "parseCallExpression";
+        registerInfix(TokenType.PLUS, "parseInfixExpression");
+        registerInfix(TokenType.MINUS, "parseInfixExpression");
+        registerInfix(TokenType.SLASH, "parseInfixExpression");
+        registerInfix(TokenType.ASTERISK, "parseInfixExpression");
+        registerInfix(TokenType.EQ, "parseInfixExpression");
+        registerInfix(TokenType.NOT_EQ, "parseInfixExpression");
+        registerInfix(TokenType.LT, "parseInfixExpression");
+        registerInfix(TokenType.GT, "parseInfixExpression");
+        registerInfix(TokenType.LPAREN, "parseCallExpression");
+        registerInfix(TokenType.LBRACKET, "parseIndexExpression");
         
-        // Read two tokens to initialize curToken and peekToken
         nextToken();
         nextToken();
+    }
+    
+    void registerPrefix(TokenType tokenType, string fn) {
+        prefixParseFns[tokenType] = fn;
+    }
+    
+    void registerInfix(TokenType tokenType, string fn) {
+        infixParseFns[tokenType] = fn;
     }
     
     void nextToken() {
@@ -86,8 +95,7 @@ class Parser {
     }
     
     LetStatement parseLetStatement() {
-        LetStatement stmt;
-        stmt.token = curToken;
+        LetStatement stmt = new LetStatement(curToken);
         
         if (!expectPeek(TokenType.IDENT)) {
             return null;
@@ -111,8 +119,7 @@ class Parser {
     }
     
     ReturnStatement parseReturnStatement() {
-        ReturnStatement stmt;
-        stmt.token = curToken;
+        ReturnStatement stmt = new ReturnStatement(curToken);
         
         nextToken();
         
@@ -125,9 +132,47 @@ class Parser {
         return stmt;
     }
     
+    ImportStatement parseImportStatement() {
+        ImportStatement stmt = new ImportStatement(curToken);
+        
+        if (!expectPeek(TokenType.IDENT)) {
+            return null;
+        }
+        
+        stmt.path = parseIdentifierPath();
+        
+        if (peekTokenIs(TokenType.AS)) {
+            nextToken();
+            if (!expectPeek(TokenType.IDENT)) {
+                return null;
+            }
+            stmt.alias = curToken.literal;
+        }
+        
+        if (!expectPeek(TokenType.SEMICOLON)) {
+            return null;
+        }
+        
+        return stmt;
+    }
+    
+    string[] parseIdentifierPath() {
+        string[] path;
+        path ~= curToken.literal;
+        
+        while (peekTokenIs(TokenType.DOT)) {
+            nextToken();
+            if (!expectPeek(TokenType.IDENT)) {
+                return null;
+            }
+            path ~= curToken.literal;
+        }
+        
+        return path;
+    }
+    
     ExpressionStatement parseExpressionStatement() {
-        ExpressionStatement stmt;
-        stmt.token = curToken;
+        ExpressionStatement stmt = new ExpressionStatement(curToken);
         
         stmt.expression = parseExpression(LOWEST);
         
@@ -136,6 +181,22 @@ class Parser {
         }
         
         return stmt;
+    }
+    
+    BlockStatement parseBlockStatement() {
+        BlockStatement block = new BlockStatement(curToken);
+        
+        nextToken();
+        
+        while (!curTokenIs(TokenType.RBRACE) && !curTokenIs(TokenType.EOF)) {
+            Statement stmt = parseStatement();
+            if (stmt !is null) {
+                block.statements ~= stmt;
+            }
+            nextToken();
+        }
+        
+        return block;
     }
     
     Expression parseExpression(Precedence precedence) {
@@ -160,7 +221,213 @@ class Parser {
         return leftExp;
     }
     
-    // ... (other parsing methods remain the same)
+    Expression parseIdentifier() {
+        return new Identifier(curToken, curToken.literal);
+    }
+    
+    Expression parseIntegerLiteral() {
+        IntegerLiteral lit = new IntegerLiteral(curToken);
+        
+        try {
+            lit.value = to!long(curToken.literal);
+        } catch (Exception e) {
+            errors ~= format("could not parse %s as integer", curToken.literal);
+            return null;
+        }
+        
+        return lit;
+    }
+    
+    Expression parseBooleanLiteral() {
+        return new BooleanLiteral(curToken, curTokenIs(TokenType.TRUE));
+    }
+    
+    Expression parseStringLiteral() {
+        return new StringLiteral(curToken, curToken.literal);
+    }
+    
+    Expression parseArrayLiteral() {
+        ArrayLiteral array = new ArrayLiteral(curToken);
+        array.elements = parseExpressionList(TokenType.RBRACKET);
+        return array;
+    }
+    
+    Expression[] parseExpressionList(TokenType end) {
+        Expression[] list;
+        
+        if (peekTokenIs(end)) {
+            nextToken();
+            return list;
+        }
+        
+        nextToken();
+        list ~= parseExpression(LOWEST);
+        
+        while (peekTokenIs(TokenType.COMMA)) {
+            nextToken();
+            nextToken();
+            list ~= parseExpression(LOWEST);
+        }
+        
+        if (!expectPeek(end)) {
+            return null;
+        }
+        
+        return list;
+    }
+    
+    Expression parsePrefixExpression() {
+        PrefixExpression expression = new PrefixExpression(curToken, curToken.literal);
+        
+        nextToken();
+        
+        expression.right = parseExpression(PREFIX);
+        
+        return expression;
+    }
+    
+    Expression parseInfixExpression(Expression left) {
+        InfixExpression expression = new InfixExpression(curToken, left, curToken.literal);
+        
+        Precedence precedence = curPrecedence();
+        nextToken();
+        expression.right = parseExpression(precedence);
+        
+        return expression;
+    }
+    
+    Expression parseGroupedExpression() {
+        nextToken();
+        
+        Expression exp = parseExpression(LOWEST);
+        
+        if (!expectPeek(TokenType.RPAREN)) {
+            return null;
+        }
+        
+        return exp;
+    }
+    
+    Expression parseIfExpression() {
+        IfExpression expression = new IfExpression(curToken);
+        
+        if (!expectPeek(TokenType.LPAREN)) {
+            return null;
+        }
+        
+        nextToken();
+        expression.condition = parseExpression(LOWEST);
+        
+        if (!expectPeek(TokenType.RPAREN)) {
+            return null;
+        }
+        
+        if (!expectPeek(TokenType.LBRACE)) {
+            return null;
+        }
+        
+        expression.consequence = parseBlockStatement();
+        
+        if (peekTokenIs(TokenType.ELSE)) {
+            nextToken();
+            
+            if (!expectPeek(TokenType.LBRACE)) {
+                return null;
+            }
+            
+            expression.alternative = parseBlockStatement();
+        }
+        
+        return expression;
+    }
+    
+    Expression parseFunctionLiteral() {
+        FunctionLiteral lit = new FunctionLiteral(curToken);
+        
+        if (!expectPeek(TokenType.LPAREN)) {
+            return null;
+        }
+        
+        lit.parameters = parseFunctionParameters();
+        
+        if (!expectPeek(TokenType.LBRACE)) {
+            return null;
+        }
+        
+        lit.body = parseBlockStatement();
+        
+        return lit;
+    }
+    
+    Identifier[] parseFunctionParameters() {
+        Identifier[] identifiers;
+        
+        if (peekTokenIs(TokenType.RPAREN)) {
+            nextToken();
+            return identifiers;
+        }
+        
+        nextToken();
+        
+        Identifier ident = new Identifier(curToken, curToken.literal);
+        identifiers ~= ident;
+        
+        while (peekTokenIs(TokenType.COMMA)) {
+            nextToken();
+            nextToken();
+            ident = new Identifier(curToken, curToken.literal);
+            identifiers ~= ident;
+        }
+        
+        if (!expectPeek(TokenType.RPAREN)) {
+            return null;
+        }
+        
+        return identifiers;
+    }
+    
+    Expression parseCallExpression(Expression function) {
+        CallExpression exp = new CallExpression(curToken, function);
+        exp.arguments = parseExpressionList(TokenType.RPAREN);
+        return exp;
+    }
+    
+    Expression parseIndexExpression(Expression left) {
+        IndexExpression exp = new IndexExpression(curToken, left);
+        
+        nextToken();
+        exp.index = parseExpression(LOWEST);
+        
+        if (!expectPeek(TokenType.RBRACKET)) {
+            return null;
+        }
+        
+        return exp;
+    }
+    
+    bool curTokenIs(TokenType t) {
+        return curToken.type == t;
+    }
+    
+    bool peekTokenIs(TokenType t) {
+        return peekToken.type == t;
+    }
+    
+    bool expectPeek(TokenType t) {
+        if (peekTokenIs(t)) {
+            nextToken();
+            return true;
+        } else {
+            peekError(t);
+            return false;
+        }
+    }
+    
+    void peekError(TokenType t) {
+        string msg = format("expected next token to be %s, got %s instead",
+                           t, peekToken.type);
+        errors ~= msg;
+    }
     
     enum Precedence {
         LOWEST,
